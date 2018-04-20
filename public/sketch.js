80dde4a13a2da3fadb8384ec05b97181b7fd744a
marrer99 <marrero99@msn.com>
2018-04-20 15:47:00 -0400

    Add files via upload

--- public/sketch.js
+++ public/sketch.js
@@ -3,16 +3,17 @@ var bx;
 var points = 0;
 var growthfactor = 0;
 var acceleration = 1;
-var ballRadius = 200;
+var ballRadius = 150;
 var score = 0;
 
 function setup() {
-    createCanvas(800,800);
+    createCanvas(ballRadius*4,ballRadius*4);
     bx= new boxed(width/2, height/2, floor(ballRadius * 4), color(255,204,0));
     outerball= new ball(width/2, height/2, ballRadius * 4, color(0,0,255));
     bball= new ball(width/2, height/2, ballRadius * 4-25, color(255,255,255));
     innerball= new ball(width/2, height/2, ballRadius, color(255,60,60));
     console.log('starting...');
+    this.getAccounts();
 }
 
 function draw() {
@@ -23,16 +24,16 @@ function draw() {
     innerball.show();
     innerball.growBall(growthfactor*acceleration);
    
-   showScore();
+   showScore(this.score);
 }
 
 
-function showScore() {
+function showScore(score) {
     noStroke();
     fill(0);
     textSize(32);
     textAlign(CENTER);
-    text('SCORE: ' + this.score, width / 2, height / 2);
+    text('SCORE: ' + score, width / 2, height / 2);
 }
 
 function keyPressed() {
@@ -72,11 +73,26 @@ function ResetPoints() {
 }
 function showPoints(jsonPoints) {
     console.log('coming back ...' +jsonPoints.TotalPoints );
-    message = 'POINTS: ' + jsonPoints.TotalPoints;
-    document.getElementById('points').innerHTML = "<b>" + message + "</b>";
+    //message = 'POINTS: ' + jsonPoints.TotalPoints;
+    //document.getElementById('points').innerHTML = "<b>" + message + "</b>";
     this.score = jsonPoints.TotalPoints;
 }
 
+function getAccounts() {
+    var url = '/Accounts';
+    result = loadJSON(url ,{},"json",loadAccounts,function(jsresponse) {console.log("error : " + jsresponse)});
+}
+
+function loadAccounts(jsAccounts) {
+    console.log('coming back ...' + jsAccounts.accounts );
+    accountSelect = document.getElementById('accounts');
+    for(i in jsAccounts.accounts) {
+        var option = document.createElement("option");
+        option.text = jsAccounts.accounts[i];
+        accountSelect.add(option);
+    }
+    document.getElementById('canvas').focus();
+}
 function resetGame() {
     points = 0;
     acceleration = 1;
@@ -84,8 +100,9 @@ function resetGame() {
     score = 0;
     ResetPoints();
     message = 'GAME OVER';
+    this.showScore(message);
     this.sc = 0;
-    document.getElementById('points').innerHTML = "<b>" + message + "</b>";
+    //document.getElementById('points').innerHTML = "<b>" + message + "</b>";
 }
 
 
