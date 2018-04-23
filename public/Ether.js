0e211e0d2b6c38761a980243da686b1c3076c82f
marrer99 <marrero99@msn.com>
2018-04-22 21:05:51 -0400

    Add files via upload

--- public/Ether.js
+++ public/Ether.js
@@ -57,17 +57,6 @@ var playerscore = playerscoreContract.new(
             ,{"constant":false,"inputs":[{"name":"points","type":"int256"}],"name":"addPoints","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}
             ,{"constant":true,"inputs":[],"name":"getPoints","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"}
             ]);
-        // var playerscore = playerscoreContract.new(
-        //     {
-        //         from: web3.eth.accounts[0], 
-        //         data: '0x608060405234801561001057600080fd5b506101db806100206000396000f300608060405260043610610057576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063617706cc1461005c578063deae0f7714610073578063f4b7095b146100a0575b600080fd5b34801561006857600080fd5b506100716100cb565b005b34801561007f57600080fd5b5061009e60048036038101908080359060200190929190505050610114565b005b3480156100ac57600080fd5b506100b5610166565b6040518082815260200191505060405180910390f35b60008060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060000181905550565b806000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000016000828254019250508190555050565b60008060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600001549050905600a165627a7a723058200e2089db5f19f93336288260dd1724bfdce30b7f0de9d974636e4e49f71e9dbb0029', 
-        //         gas: '4700000'
-        //     }, function (e, contract){
-        //         console.log(e, contract);
-        //         if (typeof contract.address !== 'undefined') {
-        //             console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-        //         }
-        //     })
     this.contract =  playerscoreContract.at('0x0D8A4BB9b25252Cc91F05d8AFDf63b240fEb8075');
     //this.contract =  playerscoreContract.at('0x7C25C3100Fc3DE3D63cac0aEdfbED99AAe36e557'); //0x7C25C3100Fc3DE3D63cac0aEdfbED99AAe36e557
     return this.contract;
